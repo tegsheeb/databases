886e9b397f1f88f163739e2159c72d6413961036
tegshee <tegshbayar@gmail.com>
2020-06-04 16:21:23 -0700

    Some progress

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -1,6 +1,43 @@
 var models = require('../models');
 
 module.exports = {
-  get: function (req, res) {}, // a function which handles a get request for all messages
-  post: function (req, res) {} // a function which handles posting a message to the database
-};
+  get: function (req, res) {
+    // use getAll function
+    models.messages.getAll((error, result) => {
+      if (error) {
+        //do something
+        res.statusCode(400);
+        res.end(error);
+      } else {
+        //do something
+        res.statusCode(200);
+        res.end('this is a message');
+      }
+    });
+  }, // a function which handles a get request for all messages
+
+  // we want to pass the data from Model.messages.getAll to user per GET request
+
+  // TODO
+
+  post: function (req, res) {
+
+    models.messages.create( req.json, (error, result) => {
+      if (error) {
+        //do something
+        res.statusCode(400);
+        res.end(error);
+      } else {
+        //do something
+        res.statusCode(200);
+        /// ??? res.end ( cb() ) ????
+        // res.send(??? send cd with result to client)
+        res.end(result);
+      }
+    });
+
+
+
+  // a function which handles posting a message to the database
+  }
+}
--- server/models/messages.js
+++ server/models/messages.js
@@ -1,10 +1,35 @@
 var db = require('../db');
 
 module.exports = {
-  getAll: function () {}, // a function which produces all the messages
-  create: function () {
-    // ???? before
+  getAll: function (callback) {
+    console.log(message);
 
-    INSERT into messages (username, text_content, roomname) Values(${request.json.username}, ${request.json.text_content}, ${request.json.roomname})
-  } // a function which can be used to insert a message into the database
+    var getSql = 'SELECT * from messages';
+
+    db.index.connection.query(getSql, function (error, results) {
+      if (error) {
+        callback(error, null);
+      } else {
+        console.log(results);
+        callback(null, results);
+      }
+    });
+
+
+  }, // a function which produces all the messages
+  create: function (message, callback) {
+    // ???? before
+    console.log(message);
+
+    var postSql = `INSERT into messages (username, text_content, roomname) Values(${message.username}, ${message.message}, ${message.roomname})`;
+
+    db.index.connection.query(postSql, function (error, results, fields) {
+      if (error) {
+        callback(error, null);
+      } else {
+        callback (null, result); //??
+      }
+    });
+  }
+  // a function which can be used to insert a message into the database
 };
